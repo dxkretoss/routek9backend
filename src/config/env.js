76e3a8792d9690434cfa986f24a66536cba5387f
rchintan405 <rchintan405@gmail.com>
2026-09-03 14:00:56 +0530

    update code make that live

--- src/config/env.js
+++ src/config/env.js
@@ -1,15 +1,15 @@
-﻿import dotenv from "dotenv";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const env = {
   nodeEnv: process.env.NODE_ENV || "development",
-  port: Number(process.env.PORT) || 5000,
+  port: Number(process.env.PORT) || 4022,
   apiPrefix: process.env.API_PREFIX || "/api/v1",
   corsOrigin: process.env.CORS_ORIGIN || "*",
   stripeSecretKey: process.env.STRIPE_SECRET_KEY || "",
-  stripePriceMonthly: process.env.STRIPE_PRICE_MONTHLY || "price_1UBCfECjtUNWPqGvQmM7HqCL",
-  stripePriceYearly: process.env.STRIPE_PRICE_YEARLY || "price_1UBCfxCjtUNWPqGvAgJkM7A5",
+  stripePriceMonthly: process.env.STRIPE_PRICE_MONTHLY || "price_1UBVwuCjtUNWPqGvUoCqDarm",
+  stripePriceYearly: process.env.STRIPE_PRICE_YEARLY || "price_1UBVxCCjtUNWPqGvUoCQ7wh4",
   supabaseUrl: process.env.SUPABASE_URL || "",
   supabaseAnonKey: process.env.SUPABASE_ANON_KEY || "",
 };
